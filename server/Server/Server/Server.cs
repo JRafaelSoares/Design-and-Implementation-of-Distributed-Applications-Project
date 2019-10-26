29589bca50b5d35c6b09335a3e28495dd0ec50fd
MVBarros <miguel.v.barros@tecnico.ulisboa.pt>
2019-10-26 02:15:04 -0700

    Organize Using

--- server/Server/Server/Server.cs
+++ server/Server/Server/Server.cs
@@ -1,12 +1,9 @@
-﻿using System;
+﻿using MSDAD.Shared;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
-using System.Text;
-using System.Threading.Tasks;
-using MSDAD.Shared;
 
 namespace MSDAD
 {
