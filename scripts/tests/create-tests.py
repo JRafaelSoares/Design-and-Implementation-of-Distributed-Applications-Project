09c176f9b403337641189b5412a4a67c2702f766
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2019-12-06 21:12:34 +0000

    master scripts

--- scripts/tests/create-tests.py
+++ scripts/tests/create-tests.py
@@ -4,7 +4,7 @@ import os
 import shutil
 
 
-num_clients = 12
+num_clients = 13
 
 test_names = ["create-one-server", "create-two-servers", "create-three-servers", "join-one-server", \
         "join-two-servers", "join-three-servers", "list-one-server", "list-two-servers", "list-three-servers", \
@@ -14,6 +14,7 @@ test_names = ["create-one-server", "create-two-servers", "create-three-servers",
 
 ''' create client files'''
 for test_name in test_names:
+    shutil.rmtree("test-scripts/client/" + test_name, ignore_errors = True)
     os.makedirs("test-scripts/client/" + test_name)
     for i in range(num_clients):
         f = open("test-scripts/client/" + test_name + "/client-" + str(i) + ".txt", "w")
